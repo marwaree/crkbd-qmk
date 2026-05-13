2a50255b4d0e2126d4800d3359ec2a9c733191d4
Marco Riola Fraga <marcoriola@proton.me>
2026-05-13 17:59:21 +0200

    update

--- keyboards/crkbd/keymaps/marwaree/keymap.c
+++ keyboards/crkbd/keymaps/marwaree/keymap.c
@@ -5,8 +5,8 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3_ex2(
-       //,-------------------------------------------------------------.  ,--------------------------------------------------------------.
-   LALT_T(KC_TAB),    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,   KC_NO,      KC_NO,    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_DEL,
+      //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
+   LALT_T(KC_TAB),    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,  KC_DEL,      KC_NO,    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_DEL,
       //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
    LCTL_T(KC_ESC),    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,   KC_NO,      KC_NO,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
       //|--------+--------+--------+--------+--------+--------+--------'  '--------+--------+--------+--------+--------+--------+--------|
@@ -17,20 +17,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
             ),
     [1] = LAYOUT_split_3x6_3_ex2(
-       //,-------------------------------------------------------------.  ,--------------------------------------------------------------.
-          KC_TRNS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,   KC_NO,      KC_NO, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS,  KC_EQL, KC_TRNS,
+      //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
+          KC_TRNS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_TRNS,      KC_NO, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS,  KC_EQL, KC_TRNS,
       //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
           KC_TRNS,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_NO,      KC_NO,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_TRNS,
       //|--------+--------+--------+--------+--------+--------+--------'  '--------+--------+--------+--------+--------+--------+--------|
           KC_TRNS,   KC_NO,   KC_NO,  KC_GRV, KC_TILD,   KC_NO,                        KC_NO, KC_BSLS, KC_PIPE, KC_UNDS, KC_PLUS, KC_TRNS,
       //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
-                                              KC_TRNS,   KC_NO, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS
+                                              KC_TRNS,   KC_NO, KC_TRNS,    KC_TRNS,  KC_DOT, KC_COMM
                                           //`--------------------------'  `--------------------------'
 
             ),
     [2] = LAYOUT_split_3x6_3_ex2(
-       //,-------------------------------------------------------------.  ,--------------------------------------------------------------.
-          KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO, KC_PGUP, KC_HOME,   KC_UP,  KC_END,   KC_NO,   KC_NO,
+      //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
+          KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS,      KC_NO, KC_PGUP, KC_HOME,   KC_UP,  KC_END,   KC_NO,   KC_NO,
       //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
           KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,   KC_NO,   KC_NO,
       //|--------+--------+--------+--------+--------+--------+--------'  '--------+--------+--------+--------+--------+--------+--------|
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
             ),
     [3] = LAYOUT_split_3x6_3_ex2(
-       //,-------------------------------------------------------------.  ,--------------------------------------------------------------.
+      //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
           KC_TRNS,   KC_NO,   KC_NO,   MS_UP, MS_WHLU,   KC_NO,   KC_NO,      KC_NO,   KC_NO, MS_WHLU,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
       //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
           KC_TRNS,   KC_NO, MS_LEFT, MS_DOWN, MS_RGHT,   KC_NO,   KC_NO,      KC_NO,   KC_NO, MS_BTN1, MS_BTN2,   KC_NO,   KC_NO,   KC_NO,
@@ -53,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
             ),
     [4] = LAYOUT_split_3x6_3_ex2(
-       //,-------------------------------------------------------------.  ,--------------------------------------------------------------.
-          KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, RM_TOGG,    RM_HUEU,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+      //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
+          KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, RM_TOGG,    RM_HUEU,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   TG(5),
       //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
           KC_TRNS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,    RM_HUED,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_NO,
       //|--------+--------+--------+--------+--------+--------+--------'  '--------+--------+--------+--------+--------+--------+--------|
@@ -63,20 +63,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                               KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS
                                           //`--------------------------'  `--------------------------'
 
+            ),
+
+
+    [5] = LAYOUT_split_3x6_3_ex2(
+      //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
+           KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_ESC,      KC_NO,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   TG(5),
+      //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+          KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_NO,      KC_NO,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      //|--------+--------+--------+--------+--------+--------+--------'  '--------+--------+--------+--------+--------+--------+--------|
+          KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+      //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
+                                                KC_NO,  KC_SPC,  KC_SPC,      KC_NO,   MO(4),   KC_NO
+                                          //`--------------------------'  `--------------------------'
+
             )};
 
+
 #define SPC_MT LGUI_T(KC_SPC)
+#define LT1_BSPC LT(1, KC_BSPC)
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (keycode == SPC_MT && record->tap.count && record->event.pressed) {
-        if (get_mods() & MOD_MASK_SHIFT) {
+    if (keycode == SPC_MT && record->event.pressed && record->tap.count) {
+        if (get_mods() & MOD_BIT(KC_LSFT)) {
             tap_code16(KC_UNDS);
+            return false;
+        }
+    }
+
+    if (keycode == LT1_BSPC && record->event.pressed && record->tap.count) {
+        if (get_mods() & MOD_BIT(KC_RALT)) {
+            tap_code16(KC_DEL);
             return false;
         }
     }
     return true;
 }
-
 
 
 const key_override_t opening_parentheses_override = ko_make_basic(MOD_BIT(KC_RALT), KC_T, KC_LPRN);
